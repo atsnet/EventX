e11d8b8191482319019ce3128f62f4c3abf94c91
atsnet <nt05271007@gmail.com>
2024-08-31 21:32:52 +0900

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 
 def readAll():
-    response = requests.get(st.secrets["readall"])
+    response = requests.get("https://atsnet.fastgenapp.com/readAll")
     return response.json()
 
 def holdDB(id):
@@ -11,7 +11,7 @@ def holdDB(id):
         'id': id,
         'hold': 'Hold',
     }
-    response = requests.post(st.secrets["holddb"], headers=headers, json=json_data)
+    response = requests.post("https://atsnet.fastgenapp.com/holdDB", headers=headers, json=json_data)
 
 def resetDB(id):
     headers = {'Content-Type': 'application/json'}
@@ -19,4 +19,4 @@ def resetDB(id):
         'id': id,
         'reset': 'Reset',
     }
-    response = requests.post(st.secrets["resetdb"], headers=headers, json=json_data)    
+    response = requests.post("https://atsnet.fastgenapp.com/resetDB", headers=headers, json=json_data)    
